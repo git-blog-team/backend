fe63eed8f09260416e19f35c8d4bfe2be1bf294e
uiop5487 <uiop5487@gmail.com>
2023-05-03 23:22:38 +0900

    first commit

---
+++ src/main/java/com/blog/blograss/BlograssApplication.java
@@ -0,0 +1,13 @@
+package com.blog.blograss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlograssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlograssApplication.class, args);
+	}
+
+}
